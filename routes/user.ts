13ef422002b1d40000a09211d336c39dee81ac6a
pbbaque <pbbaque@gmail.com>
2025-02-25 09:43:09 +0100

    password encryption implemented

--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { User } from "../models/user";
+import bcrypt from 'bcryptjs';
 
 const userRoutes = Router();
 
@@ -8,7 +9,7 @@ userRoutes.post('/create',( req : Request, res : Response ) => {
     const user = {
         name: req.body.name, 
         email: req.body.email,
-        password: req.body.password,
+        password: bcrypt.hashSync(req.body.password, 10),
         avatar: req.body.avatar
     };
 
